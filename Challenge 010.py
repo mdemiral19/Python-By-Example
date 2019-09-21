f09634fb01fdf6433ace63b28d1d0e00dfc73467
Muhammed Demiral <>
2019-09-21 21:03:00 +0200

    Create Challenge 010.py

---
+++ Challenge 010.py
@@ -0,0 +1,5 @@
+'''There are 2,204 pounds in a kilogram, Ask the user to enter a weight in kilograms and convert it to pounds.'''
+
+kilogram = float(input('Please enter the number of kg you want to convert into lb: '))
+conversion = 2.204 * kilogram
+print('This equals', conversion, 'lb.')
