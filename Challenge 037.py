500dfbc2cab3253e7c8de84dd89ea417959213e7
Muhammed Demiral <>
2019-09-24 22:47:33 +0200

    Create Challenge 037.py

---
+++ Challenge 037.py
@@ -0,0 +1,5 @@
+'''Challenge 037: Ask the user to enter their name and display each letter in their name on a separate line.'''
+
+name = input('Please enter your name: ')
+for i in name:
+    print(i)
