399d31d12d217f3a0d9c46d8af28709c0a505442
Muhammed Demiral <>
2019-09-24 22:47:08 +0200

    Create Challenge 027.py

---
+++ Challenge 027.py
@@ -0,0 +1,4 @@
+'''Challenge 027: Ask the user to enter a number with lots of decimal places. Multiply this number by two and display the answer.'''
+
+number = float(input('Enter a crazy high number: '))
+print(number*2)
