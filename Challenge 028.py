7ba5bf2473939a8da94f2f11d5c42a3c2af9ddfa
Muhammed Demiral <>
2019-09-24 22:47:10 +0200

    Create Challenge 028.py

---
+++ Challenge 028.py
@@ -0,0 +1,5 @@
+'''Challenge 028: Update program 027 so that it will display the answer to to two decimal places.'''
+
+number = float(input('Enter a crazy high number: '))
+result = number * 2
+print(round(result,2))
